51f4f6ac76e5f8ee13d12c7237ea03525b808ef2
Leon Noel <leon.a.noel+github@gmail.com>
2021-02-10 15:19:25 -0800

    working

---
+++ Beginner/reverseString/index-START.js
@@ -0,0 +1,14 @@
+/* CHALLENGE
+Given a string of text, write an algorithm that returns the text received in a reversed format. 
+E.g reverseString('algorithms') // should return 'smhtirogla'
+*/
+
+
+
+function reverseString(text) {
+    return text.split('').reverse().join('')
+}
+
+
+
+module.exports = reverseString
